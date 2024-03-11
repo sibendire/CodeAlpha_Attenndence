efd8a4331092dd1568c0e0e9e77177aa00836882
sibendire <sibendirejoshua@gmail.com>
2024-03-11 19:04:32 +0300

    created student service and repository

---
+++ src/main/java/com/attendance/attendence/attendenceservice/StudentService.java
@@ -0,0 +1,4 @@
+package com.attendance.attendence.attendenceservice;
+
+public interface StudentService {
+}
---
+++ src/main/java/com/attendance/attendence/attendenceservice/serviceimpl/StudentServiceImpl.java
@@ -0,0 +1,8 @@
+package com.attendance.attendence.attendenceservice.serviceimpl;
+
+import com.attendance.attendence.attendenceservice.StudentService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+}
